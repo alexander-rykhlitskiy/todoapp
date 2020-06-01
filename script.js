63c83f6e26e608b44c4755d23a7fc19a06c08a7e
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2020-06-01 17:42:13 +0300

    Mark as done

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const createItem = (text) => {
-    const result = document.createElement('li');
-    result.innerHTML = text;
-    return result;
-  }
-
   const form = document.querySelector('form.new_task');
   const input = form.querySelector('input.task_title');
   const tasksList = document.querySelector('#tasks');
 
+  const createCheckboxNode = () => {
+    const result = document.createElement('input');
+    result.type = 'checkbox';
+    result.className = 'done';
+    return result;
+  }
+
+  const createCheckbox = (taskNode) => {
+    const result = createCheckboxNode();
+    result.addEventListener('change', (event) => {
+      if (event.target.checked) {
+        taskNode.style.textDecoration = 'line-through';
+        tasksList.appendChild(taskNode);
+      } else {
+        taskNode.style.textDecoration = '';
+        tasksList.prepend(taskNode);
+      }
+    })
+    return result;
+  }
+
+  const createTask = (text) => {
+    const result = document.createElement('li');
+    result.className = 'task';
+
+    result.appendChild(createCheckbox(result));
+    result.append(text);
+
+    return result;
+  }
+
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    tasksList.appendChild(createItem(input.value));
+    tasksList.appendChild(createTask(input.value));
     form.reset();
   })
 })
